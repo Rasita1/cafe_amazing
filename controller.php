2914469019920eb137ade6aa634ce4955b3da1cb
Rasita1 <rasita.ro1@gmail.com>
2025-01-30 09:27:36 +0700

    Add files via upload

--- controller.php
+++ controller.php
@@ -1,84 +1,38 @@
 <?php
-
-namespace Elementor\Modules\Favorites;
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
+namespace Elementor\Modules\Checklist\Data;
 
 use Elementor\Data\V2\Base\Controller as Controller_Base;
-use Elementor\Plugin;
+use Elementor\Modules\Checklist\Data\Endpoints\Steps;
+use Elementor\Modules\Checklist\Data\Endpoints\User_Progress;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
 class Controller extends Controller_Base {
-
 	public function get_name() {
-		return 'favorites';
-	}
-
-	public function create_item( $request ) {
-		$module = $this->get_module();
-		$type = $request->get_param( 'id' );
-		$favorite = $request->get_param( 'favorite' );
-
-		$module->update( $type, $favorite, $module::ACTION_MERGE );
-
-		return $module->get( $type );
-	}
-
-	public function delete_item( $request ) {
-		$module = $this->get_module();
-		$type = $request->get_param( 'id' );
-		$favorite = $request->get_param( 'favorite' );
-
-		$module->update( $type, $favorite, $module::ACTION_DELETE );
-
-		return $module->get( $type );
-	}
-
-	public function create_item_permissions_check( $request ) {
-		return current_user_can( 'edit_posts' );
-	}
-
-	public function delete_item_permissions_check( $request ) {
-		return $this->create_item_permissions_check( $request );
-	}
-
-	/**
-	 * Get the favorites module instance.
-	 *
-	 * @return Module
-	 */
-	protected function get_module() {
-		return Plugin::instance()->modules_manager->get_modules( 'favorites' );
+		return 'checklist';
 	}
 
 	public function register_endpoints() {
-		$this->index_endpoint->register_item_route( \WP_REST_Server::CREATABLE, [
-			'id_arg_type_regex' => '[\w]+',
-			'id' => [
-				'description' => 'Type of favorites.',
-				'type' => 'string',
-				'required' => true,
-			],
-			'favorite' => [
-				'description' => 'The favorite slug to create.',
-				'type' => 'string',
-				'required' => true,
-			],
-		] );
+		$this->index_endpoint->register_item_route();
+		$this->register_endpoint( new Steps( $this ) );
+		$this->register_endpoint( new User_Progress( $this ) );
+	}
 
-		$this->index_endpoint->register_item_route( \WP_REST_Server::DELETABLE, [
-			'id_arg_type_regex' => '[\w]+',
-			'id' => [
-				'description' => 'Type of favorites.',
-				'type' => 'string',
-				'required' => true,
-			],
-			'favorite' => [
-				'description' => 'The favorite slug to delete.',
-				'type' => 'string',
-				'required' => true,
-			],
-		] );
+	public function update_items_permissions_check( $request ) {
+		return current_user_can( 'manage_options' );
+	}
+
+	public function update_item_permissions_check( $request ) {
+		return current_user_can( 'manage_options' );
+	}
+
+	public function get_item_permissions_check( $request ) {
+		return current_user_can( 'manage_options' );
+	}
+
+	public function get_items_permissions_check( $request ) {
+		return current_user_can( 'manage_options' );
 	}
 }
--- module.php
+++ module.php
@@ -1,248 +1,106 @@
 <?php
-namespace Elementor\Modules\Favorites;
+
+namespace Elementor\Modules\AtomicWidgets;
+
+use Elementor\Core\Base\Module as BaseModule;
+use Elementor\Core\Experiments\Manager as Experiments_Manager;
+use Elementor\Modules\AtomicWidgets\DynamicTags\Dynamic_Tags_Module;
+use Elementor\Modules\AtomicWidgets\PropsResolver\Transformers\Settings\Classes_Transformer;
+use Elementor\Modules\AtomicWidgets\PropsResolver\Transformers\Settings\Image_Src_Transformer;
+use Elementor\Modules\AtomicWidgets\PropsResolver\Transformers\Settings\Image_Transformer;
+use Elementor\Modules\AtomicWidgets\PropsResolver\Transformers\Primitive_Transformer;
+use Elementor\Modules\AtomicWidgets\PropsResolver\Transformers_Registry;
+use Elementor\Modules\AtomicWidgets\PropTypes\Boolean_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\Classes_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\Image_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\Image_Src_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\Number_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\String_Prop_Type;
+use Elementor\Modules\AtomicWidgets\Widgets\Atomic_Heading;
+use Elementor\Modules\AtomicWidgets\Widgets\Atomic_Image;
+use Elementor\Plugin;
+use Elementor\Widgets_Manager;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-use Elementor\Core\Base\Module as BaseModule;
-use Elementor\Core\Experiments\Manager;
-use Elementor\Modules\Favorites\Types\Widgets;
-use Elementor\Plugin;
-use http\Exception\InvalidArgumentException;
-use WP_Error;
-
 class Module extends BaseModule {
+	const EXPERIMENT_NAME = 'atomic_widgets';
 
-	/**
-	 * List of registered favorites type.
-	 *
-	 * @var Favorites_Type[]
-	 */
-	protected $types = [];
-
-	const OPTION_NAME = 'elementor_editor_user_favorites';
-
-	/**
-	 * The name of the merge action.
-	 *
-	 * @var string
-	 */
-	const ACTION_MERGE = 'merge';
-
-	/**
-	 * The name of the delete action.
-	 *
-	 * @var string
-	 */
-	const ACTION_DELETE = 'delete';
-
-	/**
-	 * Favorites module constructor.
-	 */
-	public function __construct() {
-		// Register default types
-		$this->register( Widgets::class );
-
-		$this->populate();
-
-		Plugin::instance()->data_manager_v2->register_controller( new Controller() );
-
-		add_filter( 'elementor/tracker/send_tracking_data_params', [ $this, 'add_tracking_data' ] );
-	}
-
-	/**
-	 * Add usage data related to favorites.
-	 *
-	 * @param $params
-	 *
-	 * @return array
-	 */
-	public function add_tracking_data( $params ) {
-		$params['usages']['favorites'] = $this->get();
-
-		return $params;
-	}
+	const PACKAGES = [
+		'editor-documents', // TODO: NEED to be removed once the editor will not be dependent on the documents package.
+		'editor-panels',
+		'editor-editing-panel',
+		'editor-style',
+	];
 
 	public function get_name() {
-		return 'favorites';
+		return 'atomic-widgets';
 	}
 
-	/**
-	 * Get user favorites by type.
-	 *
-	 * @param string[]|string $type
-	 *
-	 * @return array
-	 */
-	public function get( $type = null ) {
-		if ( null === $type ) {
-			$type = array_keys( $this->types );
-		}
+	public function __construct() {
+		parent::__construct();
 
-		if ( is_array( $type ) ) {
-			return array_intersect_key(
-				$this->combined(),
-				array_flip( (array) $type )
-			);
-		}
+		$this->register_experiment();
 
-		return $this->type_instance( $type )
-			->values();
-	}
+		if ( Plugin::$instance->experiments->is_feature_active( self::EXPERIMENT_NAME ) ) {
+			Dynamic_Tags_Module::instance()->register_hooks();
 
-	/**
-	 * Merge new user favorites to a type.
-	 *
-	 * @param string        $type
-	 * @param array|string  $favorites
-	 * @param bool          $store
-	 *
-	 * @return array|bool
-	 */
-	public function merge( $type, $favorites, $store = true ) {
-		return $this->update( $type, $favorites, static::ACTION_MERGE, $store );
-	}
+			( new Atomic_Styles() )->register_hooks();
 
-	/**
-	 * Delete existing favorites from a type.
-	 *
-	 * @param string        $type
-	 * @param array|string  $favorites
-	 * @param bool          $store
-	 *
-	 * @return array|int
-	 */
-	public function delete( $type, $favorites, $store = true ) {
-		return $this->update( $type, $favorites, static::ACTION_DELETE, $store );
-	}
+			add_filter( 'elementor/editor/v2/packages', fn( $packages ) => $this->add_packages( $packages ) );
+			add_filter( 'elementor/widgets/register', fn( Widgets_Manager $widgets_manager ) => $this->register_widgets( $widgets_manager ) );
+			add_action( 'elementor/atomic-widgets/settings/transformers/register', fn ( $transformers ) => $this->register_transformers( $transformers ) );
 
-	/**
-	 * Update favorites on a type by merging or deleting from it.
-	 *
-	 * @param      $type
-	 * @param      $favorites
-	 * @param      $action
-	 * @param bool $store
-	 *
-	 * @return array|boolean
-	 */
-	public function update( $type, $favorites, $action, $store = true ) {
-		$type_instance = $this->type_instance( $type );
-		$favorites = $type_instance->prepare( $favorites );
-
-		switch ( $action ) {
-			case static::ACTION_MERGE:
-				$type_instance->merge( $favorites );
-				break;
-			case static::ACTION_DELETE:
-				$type_instance->filter(
-					function( $value ) use ( $favorites ) {
-						return ! in_array( $value, $favorites, true );
-					}
-				);
-				break;
-			default:
-				$this->action_doesnt_exists( $action );
-		}
-
-		if ( $store && ! $this->store() ) {
-			return false;
-		}
-
-		return $type_instance->values();
-	}
-
-	/**
-	 * Get registered favorites type instance.
-	 *
-	 * @param string $type
-	 *
-	 * @return Favorites_Type
-	 */
-	public function type_instance( $type ) {
-		return $this->types[ $type ];
-	}
-
-	/**
-	 * Register a new type class.
-	 *
-	 * @param string $class
-	 */
-	public function register( $class ) {
-		$type_instance = new $class();
-
-		$this->types[ $type_instance->get_name() ] = $type_instance;
-	}
-
-	/**
-	 * Returns all available types keys.
-	 *
-	 * @return string[]
-	 */
-	public function available() {
-		return array_keys( $this->types );
-	}
-
-	/**
-	 * Combine favorites from all types into a single array.
-	 *
-	 * @return array
-	 */
-	protected function combined() {
-		$all = [];
-
-		foreach ( $this->types as $type ) {
-			$favorites = $type->values();
-
-			if ( ! empty( $favorites ) ) {
-				$all[ $type->get_name() ] = $favorites;
-			}
-		}
-
-		return $all;
-	}
-
-	/**
-	 * Populate all type classes with the stored data.
-	 */
-	protected function populate() {
-		$combined = $this->retrieve();
-
-		foreach ( $this->types as $key => $type ) {
-			if ( isset( $combined[ $key ] ) ) {
-				$type->merge( $combined[ $key ] );
-			}
+			add_action( 'elementor/editor/after_enqueue_scripts', fn() => $this->enqueue_scripts() );
 		}
 	}
 
-	/**
-	 * Retrieve stored user favorites types.
-	 *
-	 * @return mixed|false
-	 */
-	protected function retrieve() {
-		return get_user_option( static::OPTION_NAME );
+	private function register_experiment() {
+		Plugin::$instance->experiments->add_feature( [
+			'name' => self::EXPERIMENT_NAME,
+			'title' => esc_html__( 'Atomic Widgets', 'elementor' ),
+			'description' => esc_html__( 'Enable atomic widgets.', 'elementor' ),
+			'hidden' => true,
+			'default' => Experiments_Manager::STATE_INACTIVE,
+			'release_status' => Experiments_Manager::RELEASE_STATUS_ALPHA,
+		] );
+	}
+
+	private function add_packages( $packages ) {
+		return array_merge( $packages, self::PACKAGES );
+	}
+
+	private function register_widgets( Widgets_Manager $widgets_manager ) {
+		$widgets_manager->register( new Atomic_Heading() );
+		$widgets_manager->register( new Atomic_Image() );
+	}
+
+	private function register_transformers( Transformers_Registry $transformers ) {
+		// Primitives
+		$transformers->register( Boolean_Prop_Type::get_key(), new Primitive_Transformer() );
+		$transformers->register( Number_Prop_Type::get_key(), new Primitive_Transformer() );
+		$transformers->register( String_Prop_Type::get_key(), new Primitive_Transformer() );
+
+		// Other
+		$transformers->register( Classes_Prop_Type::get_key(), new Classes_Transformer() );
+		$transformers->register( Image_Prop_Type::get_key(), new Image_Transformer() );
+		$transformers->register( Image_Src_Prop_Type::get_key(), new Image_Src_Transformer() );
 	}
 
 	/**
-	 * Update all changes to user favorites type.
+	 * Enqueue the module scripts.
 	 *
-	 * @return int|bool
+	 * @return void
 	 */
-	protected function store() {
-		return update_user_option( get_current_user_id(), static::OPTION_NAME, $this->combined() );
-	}
-
-	/**
-	 * Throw action doesn't exist exception.
-	 *
-	 * @param string $action
-	 */
-	public function action_doesnt_exists( $action ) {
-		throw new \InvalidArgumentException( sprintf(
-			"Action '%s' to apply on favorites doesn't exists",
-			$action
-		) );
+	private function enqueue_scripts() {
+		wp_enqueue_script(
+			'elementor-atomic-widgets-editor',
+			$this->get_js_assets_url( 'atomic-widgets-editor' ),
+			[ 'elementor-editor' ],
+			ELEMENTOR_VERSION,
+			true
+		);
 	}
 }
